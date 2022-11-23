152d2f1d1a727b5ebefe158598e8afc16722745d
andrechizh8 <andrechizh.ru@yandex.ru>
2022-11-24 02:20:04 +0300

    first commit

--- conftest.py
+++ conftest.py
@@ -5,25 +5,10 @@ from selene import by, be, have
 
 @pytest.fixture()
 def browser_open():
-    browser.config.hold_browser_open = True  # Не закрывать браузер
-    browser.config.window_width = 1920  # Выставить ширину окна браузера
-    browser.config.window_height = 1080  # Выставить высоту окна браузера
-    browser.open("https://google.com").driver.maximize_window()  # Открыть браузер в полноэкранном режиме
-    print("Браузер открыт")
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.open("https://google.com").driver.maximize_window()
 
 
-@pytest.fixture()
-def page_search_1(browser_open):  # Позитивный сценарий
-    browser.element(by.name("q")).should(be.blank).type("selene").press_enter()  # Поиск "selene"
-    browser.element(by.id("search")).should(
-        have.text("User-oriented Web UI browser tests in Python"))  # Страница содержит нужный текст
-    return "Страница содержит текст 'User-oriented Web UI browser tests in Python' "
 
 
-@pytest.fixture()
-def page_search_2(browser_open):
-    browser.element('[class="gLFyf"]').should(be.blank).type("asdghagsydgkaysgdyla").press_enter()
-    browser.element('[class="s6JM6d"]').should(have.text("По запросу asdghagsydgkaysgdyla ничего не найдено. "))
-    yield "Страница содержит текст 'По запросу asdghagsydgkaysgdyla ничего не найдено.' "
-    browser.quit()
-    print('Браузер закрыт')
--- test_new.py
+++ test_new.py
@@ -1,6 +1,13 @@
-def test_positive_1(page_search_1):
-    assert page_search_1 == "Страница содержит текст 'User-oriented Web UI browser tests in Python' "
+from selene.support.shared import browser
+from selene import by, be, have
 
 
-def test_negative_1(page_search_2):
-    assert page_search_2 == "Страница содержит текст 'По запросу asdghagsydgkaysgdyla ничего не найдено.' "
+def test_positive_1(browser_open):
+    browser.element(by.name("q")).should(be.blank).type("selene").press_enter()
+    browser.element(by.id("search")).should(have.text("yashaka/selene: User-oriented Web UI browser tests in"))
+
+
+def test_negative_1(browser_open):
+    browser.element('[class="gLFyf"]').should(be.blank).type("asdghagsydgkaysgdyla").press_enter()
+    browser.element('[class="s6JM6d"]').should(have.text("По запросу asdghagsydgkaysgdyla ничего не найдено. "))
+
